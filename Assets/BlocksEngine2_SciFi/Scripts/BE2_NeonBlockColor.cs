653659c13257b775f44b2b59db421a9883b4664f
Hyun-hyee <84731666+Hyun-hyee@users.noreply.github.com>
2022-08-31 02:46:07 +0900

    scifi_theme

---
+++ Assets/BlocksEngine2_SciFi/Scripts/BE2_NeonBlockColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BE2_NeonBlockColor : MonoBehaviour
+{
+    RectTransform _parentRectTransform;
+    RectTransform _rectTransform;
+
+    void OnEnable()
+    {
+        _parentRectTransform = transform.parent.GetComponent<RectTransform>();
+        _rectTransform = transform.GetComponent<RectTransform>();
+    }
+
+    void OnValidate()
+    {
+        transform.SetAsLastSibling();
+    }
+
+    //void Start()
+    //{
+    //
+    //}
+
+    void LateUpdate()
+    {
+        _rectTransform.sizeDelta = _parentRectTransform.sizeDelta;
+    }
+}
